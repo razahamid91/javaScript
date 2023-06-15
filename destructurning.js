86dcaca3d283e05e63d051ea8a4e2253d9eace6c
Hamid Raza <razahamid9151@gmail.com>
2023-06-16 00:58:00 +0530

    https://github.com/razahamid91/javaScript.git

--- destructurning.js
+++ destructurning.js
@@ -4,3 +4,16 @@ const fruit = ["apple","banana", "mango"];
  console.log(value1);
  console.log(value2);
  console.log(value3);
+
+
+ // destructuring using spread for storing rest element in variable 2
+ 
+ console.log("using spread operator");
+
+ const fruits = ["apple","banana", "mango"];
+ const[variable1, ...variable2]=fruits; // destructuring
+
+ console.log(variable1);
+ console.log(variable2);
+
+ 
