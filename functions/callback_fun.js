744afd462d12b87729927771596191bd8d87e8ef
Hamid Raza <razahamid9151@gmail.com>
2023-06-22 00:53:48 +0530

    callback

--- functions/callback_fun.js
+++ functions/callback_fun.js
@@ -8,8 +8,10 @@ function fun(){
     console.log("this is fun function");
 }
 
-function fun2(a){
-    console.log('inside the fun2' ,a)
+function fun2(callback){
+    console.log('inside the fun2 and i am calling fun functon')
+
+    callback();
 }
 
 fun2(fun);
